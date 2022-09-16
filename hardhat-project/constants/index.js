d87787e588ea7226e3d2242fd48afd1990a89c4a
savannah8 <willengitonga.wg@gmail.com>
2022-09-16 15:44:50 +0300

    changed whitelist contract address

--- hardhat-project/constants/index.js
+++ hardhat-project/constants/index.js
@@ -1,5 +1,5 @@
 // Address of the Whitelist Contract that you deployed
-const WHITELIST_CONTRACT_ADDRESS = "0x3a62400979A56087ebD78fE44C149e9A0bF71a9E";
+const WHITELIST_CONTRACT_ADDRESS = "0x98f5c62F7b5Bc735a64942dBec9456954c7a368A";
 // URL to extract Metadata for a Crypto Dev NFT
 const METADATA_URL = "https://nft-collection-ten-beige.vercel.app/";
 
